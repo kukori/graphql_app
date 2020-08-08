8280649443735763d3fa2b83a8f78508be2b9d2a
kukori <attila.varjas@gmail.com>
2020-08-08 15:27:44 +0700

    GraphQl init

---
+++ schema/schema.js
@@ -0,0 +1,28 @@
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require('graphql');
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: { type: GraphQLString}},
+            resolve(parent, args) {
+                // args.id
+                // code to get data from db / other source
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const server = express();
+
+server.use('/graphql', graphqlHTTP({
+    schema: schema
+}));
 
 server.listen(4000, () => {
     console.log('now listening for requests on port 4000');
